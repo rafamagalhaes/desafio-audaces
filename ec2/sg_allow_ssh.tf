72cdc0b0087c7981fdbbabbc1325529589540986
rafamagalhaes <rafa.magalhaes@gmail.com>
2022-08-02 17:23:18 -0300

    Fix SG allow_ssh (Upgrade security)

--- ec2/sg_allow_ssh.tf
+++ ec2/sg_allow_ssh.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "audaces_sg_allow_ssh" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0", ]
+    cidr_blocks      = ["172.31.0.0/16", ]
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
     security_groups  = []
